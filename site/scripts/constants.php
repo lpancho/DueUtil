5a9bbdacd2e4094be32307903d9b0c22488de2f4
lpancho <leonard.pancho@gmail.com>
2022-02-08 21:13:35 +0800

    Remove site related files

--- site/scripts/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Constants used on multiple pages
-define("SERVER_INVITE","https://discord.gg/n4b94VA");
-define("OWNER","132315148487622656");
-define("BOT_INVITE","https://discordapp.com/oauth2/authorize?client_id=[REDACTED]&scope=bot&permissions=268815424");
-
-?>
